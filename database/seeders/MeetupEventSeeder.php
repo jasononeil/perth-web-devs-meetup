7f0572ed7e410993dd8c665e5ca8e1e6a5e300aa
Jason O'Neil <jason.oneil@gmail.com>
2024-09-29 21:00:44 +0800

    Add a second event to the seeder

--- database/seeders/MeetupEventSeeder.php
+++ database/seeders/MeetupEventSeeder.php
@@ -23,10 +23,22 @@ class MeetupEventSeeder extends Seeder
                 "meetup_group_id" => $group->id,
                 "name" => "First drinks!",
                 "slug" => "first-drinks-oct-2024",
-                "description" => "This is a meetup event.",
+                "description" =>
+                    "Casual drinks after work with others in the Perth Web Dev community.",
                 "location" => "Market Grounds, Perth",
-                "start_time" => "2024-09-18 17:30:00",
-                "end_time" => "2024-09-18 19:00:00",
+                "start_time" => "2024-10-16 17:30:00",
+                "end_time" => "2024-10-16 19:00:00",
+                "max_attendance" => 30,
+            ]);
+            \App\Models\MeetupEvent::create([
+                "meetup_group_id" => $group->id,
+                "name" => "Show and tell",
+                "slug" => "show-and-tell-nov-2024",
+                "description" =>
+                    "Here from a few people about what they do at work and get a look at their current projects.",
+                "location" => "To be confirmed",
+                "start_time" => "2024-11-13 17:30:00",
+                "end_time" => "2024-11-13 19:30:00",
                 "max_attendance" => 30,
             ]);
         }
