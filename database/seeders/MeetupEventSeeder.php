e35dec3fb11d1133f48d438858e73c91ae26b60e
Jason O'Neil <jason.oneil@gmail.com>
2024-09-30 13:10:25 +0800

    Improved descriptions for seed groups and events

--- database/seeders/MeetupEventSeeder.php
+++ database/seeders/MeetupEventSeeder.php
@@ -12,6 +12,26 @@ class MeetupEventSeeder extends Seeder
      */
     public function run(): void
     {
+        $description_first_drinks = <<<EOD
+Casual drinks after work with others in the Perth Web Dev community.
+
+We're meeting at Market Grounds in the city (right outside the Perth busport, a short walk from the train station).
+
+With this meetup group, I'm hoping to alternate between events with structured talks / discussions, and casual drinks like this one.
+
+This is the first of it's type! Come along.
+EOD;
+
+        $description_show_and_tell = <<<EOD
+Get a peak into what different people are working on.
+
+We'll have 3 or 4 people tell us about their role and their current project, give us a short demo or show us some code. And some time for questions.
+
+Hopefully it'll be interesting, fun, and give you a bigger picture of the different kinds of roles and projects happening in our industry.
+
+If you're interested in sharing, let me know.
+EOD;
+
         // Add a meetup on Sep 18 2024 for "perth-web-devs"
         $group = \App\Models\MeetupGroup::where(
             "slug",
@@ -23,8 +43,7 @@ class MeetupEventSeeder extends Seeder
                 "meetup_group_id" => $group->id,
                 "name" => ":first-of-type(drinks)",
                 "slug" => "first-drinks-oct-2024",
-                "description" =>
-                    "Casual drinks after work with others in the Perth Web Dev community.",
+                "description" => $description_first_drinks,
                 "location" => "Market Grounds, Perth",
                 "start_time" => "2024-10-16 17:30:00",
                 "end_time" => "2024-10-16 19:00:00",
@@ -34,8 +53,7 @@ class MeetupEventSeeder extends Seeder
                 "meetup_group_id" => $group->id,
                 "name" => "Show and tell",
                 "slug" => "show-and-tell-nov-2024",
-                "description" =>
-                    "Here from a few people about what they do at work and get a look at their current projects.",
+                "description" => $description_show_and_tell,
                 "location" => "To be confirmed",
                 "start_time" => "2024-11-13 17:30:00",
                 "end_time" => "2024-11-13 19:30:00",
--- database/seeders/MeetupGroupSeeder.php
+++ database/seeders/MeetupGroupSeeder.php
@@ -13,11 +13,16 @@ class MeetupGroupSeeder extends Seeder
      */
     public function run()
     {
+        $description = <<<EOD
+A Perth meetup group for web developers, software engineers, designers, PMs and whoever else is building things for the web.
+
+We'll have a mix of social catch-ups and meetups with technical talks or discussions about our industry.
+EOD;
+
         MeetupGroup::create([
             "slug" => "perth-web-devs",
             "name" => "Perth Web Devs",
-            "description" =>
-                "A meetup group for web developers in Perth, Western Australia. A mix of social catch-ups and meetups with technical talks.",
+            "description" => $description,
             // Add any other fields you need here.
         ]);
     }
