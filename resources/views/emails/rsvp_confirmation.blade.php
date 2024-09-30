4bb72a5e2038ca84a39306439bf61f9a9917a10c
Jason O'Neil <jason.oneil@gmail.com>
2024-09-30 22:48:55 +0800

    Fixes to confirmation email

--- resources/views/emails/rsvp_confirmation.blade.php
+++ resources/views/emails/rsvp_confirmation.blade.php
@@ -5,12 +5,13 @@ Dear {{ $name }},
 
 Thank you for your RSVP to the {{ $event->name }} event of the {{ $group->name }} group.
 
-The event will be held on {{ $event->date }} at {{ $event->location }}.
+The event will be held on {{ $event->formattedDate() }} at {{ $event->formattedTime() }}, meeting at {{ $event->location }}.
 
 We're excited to see you there!
 
 If you need to change your RSVP, or have questions, just reply to this email and let me know. (I haven't automated any of that yet).
 
 Thanks,<br>
+Jason O'Neil<br>
 {{ config('app.name') }}
 </x-mail::message>
