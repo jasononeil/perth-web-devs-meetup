0f66d40b923c790b40551396c0f83f45eb285d42
Jason O'Neil <jason.oneil@gmail.com>
2025-08-13 11:56:36 +0800

    Create mail types for bumps

---
+++ app/Mail/EventBump.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\MeetupEvent;
+use App\Models\MeetupGroup;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class EventBump extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(
+        public MeetupEvent $event,
+        public MeetupGroup $group,
+        public string $rsvpUrl,
+        public string $customMessage,
+    ) {
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            from: new Address(
+                "jason@jasononeil.au",
+                "Jason O'Neil (Perth Web Devs meetup)"
+            ),
+            subject: "Reminder: {$this->event->name} ({$this->group->name})"
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(markdown: "emails.event_bump");
+    }
+
+    public function attachments(): array
+    {
+        return [];
+    }
+}
---
+++ app/Mail/EventReminder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\MeetupEvent;
+use App\Models\MeetupGroup;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class EventReminder extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public function __construct(
+        public MeetupEvent $event,
+        public MeetupGroup $group,
+        public string $rsvpUrl,
+    ) {
+    }
+
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            from: new Address(
+                "jason@jasononeil.au",
+                "Jason O'Neil (Perth Web Devs meetup)"
+            ),
+            subject: "Tomorrow: {$this->event->name} ({$this->group->name})"
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(markdown: "emails.event_reminder");
+    }
+
+    public function attachments(): array
+    {
+        return [];
+    }
+}
