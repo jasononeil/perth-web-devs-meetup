197c67d38aea3caf117c138cd697ae28b84660a4
Jason O'Neil <jason.oneil@gmail.com>
2025-08-13 13:09:57 +0800

    Fix event filterin in SendEventAnnouncement command

--- app/Console/Commands/SendEventAnnouncement.php
+++ app/Console/Commands/SendEventAnnouncement.php
@@ -28,14 +28,14 @@ class SendEventAnnouncement extends Command
         $group = $this->choice(
             "Which group is the event for?",
             $groups->pluck("name")->toArray(),
-            0
+            0,
         );
         $group = $groups->where("name", $group)->first();
 
         // Get upcoming events for the group
         $upcomingEvents = MeetupEvent::where("meetup_group_id", $group->id)
-            ->where("date", ">=", Carbon::now())
-            ->orderBy("date")
+            ->where("start_time", ">=", Carbon::now())
+            ->orderBy("start_time")
             ->get();
 
         if ($upcomingEvents->isEmpty()) {
@@ -48,10 +48,10 @@ class SendEventAnnouncement extends Command
             "Which event would you like to announce?",
             $upcomingEvents
                 ->map(
-                    fn($event) => "{$event->name} ({$event->formattedDate()})"
+                    fn($event) => "{$event->name} ({$event->formattedDate()})",
                 )
                 ->toArray(),
-            0
+            0,
         );
         $event = $upcomingEvents
             ->filter(fn($e) => "{$e->name} ({$e->formattedDate()})" === $event)
@@ -62,18 +62,18 @@ class SendEventAnnouncement extends Command
 
         // Get test email address
         $testEmail = $this->ask(
-            "Enter an email address to send a test email to (optional)"
+            "Enter an email address to send a test email to (optional)",
         );
 
         if ($testEmail) {
             $this->info("Sending test email to {$testEmail}...");
             Mail::to($testEmail)->send(
-                new NewEventAnnouncement($event, $group, $rsvpUrl)
+                new NewEventAnnouncement($event, $group, $rsvpUrl),
             );
 
             if (
                 !$this->confirm(
-                    "Did the test email look good? Would you like to proceed?"
+                    "Did the test email look good? Would you like to proceed?",
                 )
             ) {
                 $this->info("Operation cancelled.");
@@ -93,12 +93,12 @@ class SendEventAnnouncement extends Command
         $this->info("Found {$subscribers->count()} subscribers:");
         $this->table(
             ["Email"],
-            $subscribers->map(fn($sub) => [$sub->email])->toArray()
+            $subscribers->map(fn($sub) => [$sub->email])->toArray(),
         );
 
         if (
             !$this->confirm(
-                "Do you want to send the announcement to these subscribers?"
+                "Do you want to send the announcement to these subscribers?",
             )
         ) {
             $this->info("Operation cancelled.");
@@ -111,7 +111,7 @@ class SendEventAnnouncement extends Command
 
         foreach ($subscribers as $subscriber) {
             Mail::to($subscriber->email)->send(
-                new NewEventAnnouncement($event, $group, $rsvpUrl)
+                new NewEventAnnouncement($event, $group, $rsvpUrl),
             );
             $bar->advance();
         }
@@ -119,7 +119,7 @@ class SendEventAnnouncement extends Command
         $bar->finish();
         $this->newLine(2);
         $this->info(
-            "{$subscribers->count()} Announcement emails have been sent successfully!"
+            "{$subscribers->count()} Announcement emails have been sent successfully!",
         );
 
         return 0;
