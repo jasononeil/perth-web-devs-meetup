4aa800b45ce787ee9416c62f19cbe377e4c462bb
Jason O'Neil <jason.oneil@gmail.com>
2025-11-05 07:10:08 +0800

    Some minor formatting diffs

--- app/Http/Controllers/MeetupGroupController.php
+++ app/Http/Controllers/MeetupGroupController.php
@@ -4,18 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\MeetupGroup;
 use App\Models\RSVP;
+use App\Models\Person;
+use App\Models\Subscriber;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\URL;
 use App\Mail\RsvpConfirmation;
+use App\Mail\VerifyEmail;
 
 class MeetupGroupController extends Controller
 {
     public function show($groupSlug)
     {
         $group = MeetupGroup::where("slug", $groupSlug)
-            ->with(['meetupEvents' => function($query) {
-                $query->orderBy('start_time', 'desc');
-            }])
+            ->with([
+                "meetupEvents" => function ($query) {
+                    $query->orderBy("start_time", "desc");
+                },
+            ])
             ->first();
 
         if (!$group) {
@@ -23,18 +29,18 @@ class MeetupGroupController extends Controller
         }
 
         // Separate upcoming and archived events
-        $upcomingEvents = $group->meetupEvents->filter(function($event) {
+        $upcomingEvents = $group->meetupEvents->filter(function ($event) {
             return !$event->isArchived();
         });
 
-        $archivedEvents = $group->meetupEvents->filter(function($event) {
+        $archivedEvents = $group->meetupEvents->filter(function ($event) {
             return $event->isArchived();
         });
 
         return view("meetup_group.show", [
             "group" => $group,
             "upcomingEvents" => $upcomingEvents,
-            "archivedEvents" => $archivedEvents
+            "archivedEvents" => $archivedEvents,
         ]);
     }
 
@@ -56,23 +62,16 @@ class MeetupGroupController extends Controller
             ->where("slug", $eventSlug)
             ->firstOrFail();
 
-        $request->validate([
-            "name" => "required|not_regex:/http/i",
-            "email" => "nullable|email|required_without:mobile_number",
-            "mobile_number" => "nullable|required_without:email",
-        ], [
-            "name.not_regex" => "We don't allow any submissions if your name contains the substring `http`. Sorry if you validly have this as your name!"
-        ]);
-
-        RSVP::updateOrCreate(
+        $request->validate(
             [
-                "meetup_event_id" => $event->id,
-                "email" => $request->email,
-                "mobile_number" => $request->mobile_number,
+                "name" => "required|not_regex:/http/i",
+                "email" => "nullable|email|required_without:mobile_number",
+                "mobile_number" => "nullable|required_without:email",
             ],
             [
-                "name" => $request->name,
-            ]
+                "name.not_regex" =>
+                    "We don't allow any submissions if your name contains the substring `http`. Sorry if you validly have this as your name!",
+            ],
         );
 
         if ($request->has("subscribe")) {
@@ -87,16 +86,17 @@ class MeetupGroupController extends Controller
             );
         }
 
-        return redirect()
-            ->route("showEvent", [
-                "groupSlug" => $groupSlug,
-                "eventSlug" => $eventSlug,
-            ])
-            ->with(
-                "message",
-                "Thank you for your RSVP! We're excited to see you there."
-            )
-            ->with("rsvp_success", true);
+            return redirect()
+                ->route("showEvent", [
+                    "groupSlug" => $groupSlug,
+                    "eventSlug" => $eventSlug,
+                ])
+                ->with(
+                    "message",
+                    "Thank you for your RSVP! We're excited to see you there.",
+                )
+                ->with("rsvp_success", true);
+        }
     }
 
     public function subscribe(Request $request, $groupSlug)
@@ -104,13 +104,13 @@ class MeetupGroupController extends Controller
         $group = MeetupGroup::where("slug", $groupSlug)->firstOrFail();
 
         // Honeypot check - if the bot filled out the hidden field, silently redirect
-        if ($request->filled('website')) {
+        if ($request->filled("website")) {
             return redirect()->route("showGroup", ["groupSlug" => $groupSlug]);
         }
 
         // Time check - form submissions faster than 2 seconds are likely bots
-        $formRenderedAt = $request->input('_rendered_at');
-        if ($formRenderedAt && (time() - intval($formRenderedAt) < 2)) {
+        $formRenderedAt = $request->input("_rendered_at");
+        if ($formRenderedAt && time() - intval($formRenderedAt) < 2) {
             return redirect()->route("showGroup", ["groupSlug" => $groupSlug]);
         }
 
