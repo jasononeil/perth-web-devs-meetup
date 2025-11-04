192eeacff1f020cf851a54ef2f6da5c879edf94c
Jason O'Neil <jason.oneil@gmail.com>
2025-11-05 07:03:46 +0800

    Add VerifyEmail email template

---
+++ resources/views/emails/verify_email.blade.php
@@ -0,0 +1,28 @@
+<x-mail::message>
+# Verify Your Email Address
+
+Thanks for your interest in Perth Web Devs!
+
+Due to weird spammers who think filling out fake RSVPs is going to get them traffic, we need to verify your email address.
+
+@if($type === 'rsvp')
+Your RSVP will to **{{ $event->name }}** ({{ $group->name }}) will be confirmed once you click the "Verify Email Address" button below.
+@else
+Your subscription to **{{ $group->name }}** will be confirmed once you click the "Verify Email Address" button below.
+@endif
+
+Please click the button below to verify your email address and complete your {{ $type === 'rsvp' ? 'RSVP' : 'subscription' }}.
+
+<x-mail::button :url="$verificationUrl">
+Verify Email Address
+</x-mail::button>
+
+This link will expire in 48 hours.
+
+If you didn't make this request, you can safely ignore this email.
+
+Thanks,<br>
+Jason O'Neil<br>
+{{ $group->name }}
+
+</x-mail::message>
